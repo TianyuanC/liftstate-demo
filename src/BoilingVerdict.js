6c1d77d17abb4d4fc014edfca847ae0608b94a3e
Ryan Chu <tianyuan.chu@gmail.com>
2019-03-23 10:07:26 -0700

    working

---
+++ src/BoilingVerdict.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const BoilingVerdict = props => {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>;
+    }
+    return <p>The water would not boil.</p>;
+};
